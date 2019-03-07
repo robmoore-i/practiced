9de070bba54d3bf1954915496c41948abdc70bb8
Rob Moore <robmoore121@gmail.com>
2019-03-07 19:57:22 +0000

    extract conjugate_en method on Verb

--- prompt-generators/georgian/src/software_pidgin.py
+++ prompt-generators/georgian/src/software_pidgin.py
@@ -3,15 +3,12 @@ import person as person_markers
 def translate_prompt_en_ge(person, screeve, verb, noun):
     if person == person_markers.I:
         verb_ge = "ვწერ"
-        verb_en = "write"
     elif person == person_markers.YOU:
         verb_ge = "წერ"
-        verb_en = "write"
     else:
         verb_ge = "წერს"
-        verb_en = "writes"
 
     return {
-        "prompt": "Translate \"" + person.en + " " + verb_en + " code\"",
+        "prompt": "Translate \"" + person.en + " " + verb.conjugate_en(person) + " code\"",
         "answer": verb_ge + " კოდი"
     }
--- prompt-generators/georgian/src/verb.py
+++ prompt-generators/georgian/src/verb.py
@@ -1,1 +1,15 @@
-WRITE = "write"
+import person as person_markers
+
+class Verb:
+    def __init__(self):
+        pass
+    
+    def conjugate_en(self, person):
+        if person == person_markers.I:
+            return "write"
+        elif person == person_markers.YOU:
+            return "write"
+        else:
+            return "writes"
+
+WRITE = Verb()
