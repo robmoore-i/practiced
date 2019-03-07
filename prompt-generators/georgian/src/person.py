4ee3f93ed34e9ed414ce3c8e835b8b8ba623dd1b
Rob Moore <robmoore121@gmail.com>
2019-03-07 19:53:15 +0000

    Nevermind, didn't think about it

--- prompt-generators/georgian/src/person.py
+++ prompt-generators/georgian/src/person.py
@@ -1,14 +1,7 @@
-class Number:
-    def __init__(self, description):
-        self.description = description
-
-SINGULAR = Number("one")
-PLURAL = Number("multiple")
-
 class Person:
-    def __init__(self, en, number):
+    def __init__(self, en):
         self.en = en
 
-I = Person("I", SINGULAR)
-YOU = Person("You", SINGULAR)
-SHE = Person("She", SINGULAR)
+I = Person("I")
+YOU = Person("You")
+SHE = Person("She")
