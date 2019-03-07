7c4a59b7c0d16e824ee018bd44c94fa756dc4bf9
Rob Moore <robmoore121@gmail.com>
2019-03-07 19:52:41 +0000

    Make Number class (sing/plural)

--- prompt-generators/georgian/src/person.py
+++ prompt-generators/georgian/src/person.py
@@ -1,7 +1,14 @@
+class Number:
+    def __init__(self, description):
+        self.description = description
+
+SINGULAR = Number("one")
+PLURAL = Number("multiple")
+
 class Person:
-    def __init__(self, en):
+    def __init__(self, en, number):
         self.en = en
 
-I = Person("I")
-YOU = Person("You")
-SHE = Person("She")
+I = Person("I", SINGULAR)
+YOU = Person("You", SINGULAR)
+SHE = Person("She", SINGULAR)
