53a0c93535348cb9f5fe08039e70e8c4b01d4da9
Rob Moore <robmoore121@gmail.com>
2019-03-07 18:56:55 +0000

    Add type test for prompt generator. Add coloured test output

--- prompt-generators/georgian/run_tests.sh
+++ prompt-generators/georgian/run_tests.sh
@@ -2,5 +2,13 @@ function run_test() {
     PYTHONPATH=$PYTHONPATH:src python3 $1    
 }
 
+set -e
+
 run_test test/generate_prompt_test.py
 run_test test/transient_verbs_test.py
+
+set +e
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "\n${GREEN}=== All tests passed ===${NC}\n"
--- prompt-generators/georgian/test/generate_prompt_test.py
+++ prompt-generators/georgian/test/generate_prompt_test.py
@@ -6,11 +6,16 @@ from generate_prompt import generate_prompt, prompt_text
 def assert_prompt_has_correct_json_format(prompt):
     assert_that(sorted(list(prompt.keys()))).is_equal_to(["answer", "prompt"])
 
+def assert_prompt_values_have_correct_type(prompt):
+    assert_that(prompt["prompt"]).is_type_of(str)
+    assert_that(prompt["answer"]).is_type_of(str)
+
 def assert_printed_output_is_valid_stringified_json(printed_output, prompt):
     assert_that(json.loads(printed_output)).is_equal_to(prompt)
 
 def test_prompt(printed_output, prompt):
     assert_prompt_has_correct_json_format(prompt)
+    assert_prompt_values_have_correct_type(prompt)
     assert_printed_output_is_valid_stringified_json(printed_output, prompt)
 
 # Run tests
