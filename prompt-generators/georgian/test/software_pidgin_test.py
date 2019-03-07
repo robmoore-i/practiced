429a5ecc43b6974df9ce6c039a246a68beaaf11a
Rob Moore <robmoore121@gmail.com>
2019-03-07 20:07:01 +0000

    Parameterise tests for software-pidgin

--- prompt-generators/georgian/test/software_pidgin_test.py
+++ prompt-generators/georgian/test/software_pidgin_test.py
@@ -11,21 +11,17 @@ def assert_translation_for_present_writing_of_code(person, en, ge):
     assert_that(prompt["prompt"]).is_equal_to("Translate \"" + en + "\"")
     assert_that(prompt["answer"]).is_equal_to(ge)
 
-def assert_generates_I_write_code():
-    assert_translation_for_present_writing_of_code(person_marker.I, "I write code", "ვწერ კოდი")
+def assert_translations_for_present_writing_of_code():
+    inputs = [
+        (person_marker.I, "I write code", "ვწერ კოდი"),
+        (person_marker.YOU, "You write code", "წერ კოდი"),
+        (person_marker.SHE, "She writes code", "წერს კოდი"),
+        (person_marker.HE, "He writes code", "წერს კოდი")
+    ]
 
-def assert_generates_you_write_code():
-    assert_translation_for_present_writing_of_code(person_marker.YOU, "You write code", "წერ კოდი")
-    
-def assert_generates_she_writes_code():
-    assert_translation_for_present_writing_of_code(person_marker.SHE, "She writes code", "წერს კოდი")
+    for (person, en, ge) in inputs:
+        assert_translation_for_present_writing_of_code(person, en, ge)
 
-def assert_generates_he_writes_code():
-    assert_translation_for_present_writing_of_code(person_marker.HE, "He writes code", "წერს კოდი")
-    
-assert_generates_I_write_code()
-assert_generates_you_write_code()
-assert_generates_she_writes_code()
-assert_generates_he_writes_code()
+assert_translations_for_present_writing_of_code()
 
 print(__file__ + " ✔️")
