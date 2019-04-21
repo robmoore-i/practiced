dea19382a32cf49f7d4a8ce6d25ca3e706ea2231
Rob Moore <robmoore121@gmail.com>
2019-04-21 10:21:05 +0100

    Create class VocabPromptGenerator to wrap current vocab functionality so that the prompt generator can be appropriately used in tests

--- prompt-generators/georgian/src/vocab/vocab.py
+++ prompt-generators/georgian/src/vocab/vocab.py
@@ -1,33 +1,43 @@
+import os
 import random
 from vocab.vocab_prompter import VocabPrompter
 
-def vocab_file(basename):
-    return "prompt-generators/georgian/src/vocab/vocab_lists/" + basename
+class VocabPromptGenerator:
+    def __init__(self, vocab_list_dir):
+        self.vocab_list_dir = vocab_list_dir
 
-noun_prompter = VocabPrompter(
-    "What is the georgian noun",
-    "What is the english noun",
-    json_file_name=vocab_file("nouns.json"))
+    def vocab_file(self, basename):
+        return os.path.join(self.vocab_list_dir, basename)
 
-adjective_prompter = VocabPrompter(
-    "What is the georgian adjective",
-    "What is the english adjective",
-    json_file_name=vocab_file("adjectives.json"))
+    def generate_vocab_prompt(self):
+        noun_prompter = VocabPrompter(
+            "What is the georgian noun",
+            "What is the english noun",
+            json_file_name=self.vocab_file("nouns.json"))
 
-transient_verb_prompter = VocabPrompter(
-    "What is the georgian neutral form for",
-    "What is the english for",
-    json_file_name=vocab_file("transient_verbs.json"))
+        adjective_prompter = VocabPrompter(
+            "What is the georgian adjective",
+            "What is the english adjective",
+            json_file_name=self.vocab_file("adjectives.json"))
 
-phrase_prompter = VocabPrompter(
-    "როგორაა ქართულად",
-    "How do you say in english",
-    json_file_name=vocab_file("phrases.json"))
+        transient_verb_prompter = VocabPrompter(
+            "What is the georgian neutral form for",
+            "What is the english for",
+            json_file_name=self.vocab_file("transient_verbs.json"))
+
+        phrase_prompter = VocabPrompter(
+            "როგორაა ქართულად",
+            "How do you say in english",
+            json_file_name=self.vocab_file("phrases.json"))
+
+        return random.choice([
+            noun_prompter,
+            adjective_prompter,
+            transient_verb_prompter,
+            phrase_prompter
+        ]).translate_prompt()
+
+prompt_generator = VocabPromptGenerator("prompt-generators/georgian/src/vocab/vocab_lists")
 
 def generate_vocab_prompt():
-    return random.choice([
-        noun_prompter,
-        adjective_prompter,
-        transient_verb_prompter,
-        phrase_prompter
-    ]).translate_prompt()
+    return prompt_generator.generate_vocab_prompt()
