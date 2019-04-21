6eeee9e2dedfba2e86358cfce05b574950e532ef
Rob Moore <robmoore121@gmail.com>
2019-04-21 10:13:32 +0100

    Extract vocab_file function to remove long filename dupl

--- prompt-generators/georgian/src/vocab/vocab.py
+++ prompt-generators/georgian/src/vocab/vocab.py
@@ -1,25 +1,28 @@
 import random
 from vocab.vocab_prompter import VocabPrompter
 
+def vocab_file(basename):
+    return "prompt-generators/georgian/src/vocab/vocab_lists/" + basename
+
 noun_prompter = VocabPrompter(
     "What is the georgian noun",
     "What is the english noun",
-    json_file_name="prompt-generators/georgian/src/vocab/vocab_lists/nouns.json")
+    json_file_name=vocab_file("nouns.json"))
 
 adjective_prompter = VocabPrompter(
     "What is the georgian adjective",
     "What is the english adjective",
-    json_file_name="prompt-generators/georgian/src/vocab/vocab_lists/adjectives.json")
+    json_file_name=vocab_file("adjectives.json"))
 
 transient_verb_prompter = VocabPrompter(
     "What is the georgian neutral form for",
     "What is the english for",
-    json_file_name="prompt-generators/georgian/src/vocab/vocab_lists/transient_verbs.json")
+    json_file_name=vocab_file("transient_verbs.json"))
 
 phrase_prompter = VocabPrompter(
-    "How do you say in english",
     "როგორაა ქართულად",
-    json_file_name="prompt-generators/georgian/src/vocab/vocab_lists/phrases.json")
+    "How do you say in english",
+    json_file_name=vocab_file("phrases.json"))
 
 def generate_vocab_prompt():
     return random.choice([
