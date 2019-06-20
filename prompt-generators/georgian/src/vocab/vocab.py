ff1886ef3ffc071c935e8770f741c2c2e4b767e3
romo <robert.moore@zuhlke.com>
2019-06-20 12:53:05 +0100

    refactor verb_prompter(verb_type) method for extensibility

--- prompt-generators/georgian/src/vocab/vocab.py
+++ prompt-generators/georgian/src/vocab/vocab.py
@@ -18,29 +18,42 @@ class VocabPromptGenerator:
             "What is the english noun",
             json_file_name=vocab_file_path)
 
+    def verb_prompter(self, verb_type):
+        vocab_file_basename = "verbs_" + verb_type + ".json"
+        vocab_file_path = self.vocab_file(vocab_file_basename)
+        return VocabPrompter(
+            "What is the georgian neutral form for",
+            "What is the english for",
+            json_file_name=vocab_file_path)
+
+    # noinspection PyUnusedLocal
     def generate_prompt(self):
         adjective_prompter = VocabPrompter(
             "What is the georgian adjective",
             "What is the english adjective",
             json_file_name=self.vocab_file("adjectives.json"))
 
-        transient_verb_prompter = VocabPrompter(
-            "What is the georgian neutral form for",
-            "What is the english for",
-            json_file_name=self.vocab_file("transient_verbs.json"))
-
         phrase_prompter = VocabPrompter(
             "როგორაა ქართულად",
             "How do you say in english",
             json_file_name=self.vocab_file("phrases.json"))
 
-        return random.choice([
+        noun_prompters = [
             self.noun_prompter("general"),
             self.noun_prompter("time"),
             self.noun_prompter("food_and_drink"),
             self.noun_prompter("animals"),
-            self.noun_prompter("nations"),
-            adjective_prompter,
-            transient_verb_prompter,
-            phrase_prompter
-        ]).translate_prompt()
+            self.noun_prompter("nations")
+        ]
+        verb_prompters = [
+            self.verb_prompter("transient")
+        ]
+        other_prompters = [
+            phrase_prompter,
+            adjective_prompter
+        ]
+        all_prompters = noun_prompters + verb_prompters + other_prompters
+
+        selected_prompters = verb_prompters
+
+        return random.choice(selected_prompters).translate_prompt()
